17decb618937caab0050ea63884b7d61b4782b61
Tiakiana <takana@exelion.dk>
2017-02-24 22:33:37 +0100

    First Commit

---
+++ New Unity Project/Assets/Scripts/GameController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+//using System;
+
+public class GameController : MonoBehaviour {
+    public Text ChoiceA;
+    public Text ChoiceB;
+    public Text ChoiceC;
+
+    public Text NexusText;
+    public Image Image;
+    public GameObject CurrentNexus;
+
+    public GameObject[] Nexi = new GameObject[5];
+    public void LoadNexi() {
+        List<GameObject> gos = new List<GameObject>(Nexi);
+        int x = 0;
+        x = Random.Range(0,gos.Count);
+        CurrentNexus = gos[x];
+        gos.RemoveAt(x);
+                 
+        for (int i = 0; i < Nexi.Length; i++)
+        {
+
+            x = Random.Range(0,Nexi.Length);
+
+        }
+    }
+
+	void Start () {
+        ChoiceA.text = "Thomas er sej";
+        
+
+	}
+	
+	void Update () {
+	
+	}
+}
---
+++ New Unity Project/Assets/Scripts/NexusPoint.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NexusPoint : MonoBehaviour {
+    [TextArea(3, 10)]
+    public string NexusText;
+    [TextArea(3, 10)]
+    public string ChoiceAText;
+    [TextArea(3, 10)]
+    public string ChoiceBText;
+    [TextArea(3, 10)]
+    public string ChoiceCText;
+    
+    public int[] Chances = new int[3];
+    [TextArea(3, 10)]
+    public string ChoiceASuccess;
+    [TextArea(3, 10)]
+    public string ChoiceAFailure;
+    [TextArea(3, 10)]
+    public string ChoiceBSuccess;
+    [TextArea(3, 10)]
+    public string ChoiceBFailure;
+    [TextArea(3, 10)]
+    public string ChoiceCSuccess;
+    [TextArea(3, 10)]
+    public string ChoiceCFailure;
+
+    public GameObject NextChoice;
+
+    void Start () {
+        for (int i = 0; i < Chances.Length; i++)
+        {
+            Chances[i] = Random.Range(1, 101);
+        }
+            
+        
+	}
+	
+	void Update () {
+	
+	}
+}
