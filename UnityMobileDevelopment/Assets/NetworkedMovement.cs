fc16152a96fa2976c9f0730a8ad0dd2c31353d9c
Tiakiana <takana@exelion.dk>
2017-02-24 10:58:57 +0100

    Multiplayer FPS Plus Card Game Helper
    Første er ikke min, nummer to er.

---
+++ UnityMobileDevelopment/Assets/NetworkedMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class NetworkedMovement : NetworkBehaviour {
+    
+
+
+	void Start () {
+	
+	}
+	
+	
+	void Update () {
+
+        Debug.Log("What what" + isLocalPlayer);
+
+    if (isLocalPlayer)
+	    {
+	        if (Input.GetKey("a"))
+	        {
+	            transform.position -= transform.right;
+	        }
+	    }
+	}
+}
