dd6f932bedf3cd8d7f3a9a73450b078db51c629f
Tiakiana <takana@exelion.dk>
2017-07-16 17:40:47 +0200

    Lidt ekstra

--- AdventurerKings/Assets/Scripts/GameController.cs
+++ AdventurerKings/Assets/Scripts/GameController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
     public List<Market> Markets = new List<Market>();
-
+    public Player player;
+    public Market CurrentMarket;
+    public stuf make the stuff happen t
 
 
 	// Use this for initialization
@@ -14,13 +16,13 @@ public class GameController : MonoBehaviour {
 
 
     public void StartGame() {
-        GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube); 
+        /*GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube); 
 
         go.AddComponent<Market>().SetupMarket("Wishmerhill", Random.Range(1,7),Random.Range(0,15));
         go.GetComponent<Market>().xpos = 0;
         go.GetComponent<Market>().ypos = 0;
         go.transform.position = new Vector3(0, 0, 0);
-
+        */
 
     }
 	// Update is called once per frame
